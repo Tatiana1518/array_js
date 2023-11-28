b74542ab0cd2b406c074f68407fc6a7ddefa1183
Tatiana1518 <citaosa321@gmail.com>
2023-11-27 21:55:23 -0500

    Ejercicios con Array

--- script.js
+++ script.js
@@ -25,4 +25,81 @@ document.write("<h3>LA LONGITUD DE UN ARRAY</h3>")
 
 const frutas = ["manzana", "pera", "plátano", "fresa"]
 document.write("El array de frutas es:" +frutas+ "<br>")
-document.write("El arrayS de frutas tiene una longitud de: "+frutas.length+ "<br>")
+document.write("El arrays de frutas tiene una longitud de: "+frutas.length+ "<br>")
+
+//añadir un elemento, eliminarlo, buscarlo, etc.
+
+document.write("<h2>METODOS DE ARRAY</h2>")
+
+//Añadir un elemento al final
+
+document.write(("<h4> .push() </h4>"))
+
+const fruta = ["plátano", "fresa"]
+
+document.write("El array de fruta es:" +fruta.push("naranja")+"<br>")
+document.write("El array de fruta añadido es: "+frutas+"<br>")
+
+//Devuelve la nueva longitud del array
+
+document.write("el array de fruta es:" +fruta.length+"<br>")
+
+const newLength = fruta.push("naranja")
+document.write("El array de: " +newLength+ "<br>")
+document.write("El array de fruta: " +fruta+ "<br>")
+
+//elimina y devuelve el último elemento de un array
+
+document.write(("<h4> .pop() </h4>"))
+
+const frutass = ["plátano", "fresa", "naranja"]
+const ultimaFruta = frutas.pop()
+
+document.write("El array de frutass es: "+frutass+ ("<br>"))
+document.write("El array de utimafruta es: "+ultimaFruta+ ("<br>"))
+
+
+//elimina y devuelve el primer elemento de un array
+
+document.write(("<h4> .shift() </h4>"))
+
+const primeraFruta = frutass.shift()
+
+document.write("El array de frutass es: "+frutass+ ("<br>"))
+document.write("El array de la primera fruta es: "+primeraFruta+ ("<br>"))
+
+//añade un elemento al principio de un array
+
+document.write(("<h4> .unshift() </h4>"))
+
+document.write("El array añade una fruta es: "+frutass.unshift("manzana")+ ("<br>"))
+document.write("El array de frutass es: "+frutass+ ("<br>"))
+
+//Podemos concatenar dos arrays usando el método concat()
+
+
+document.write(("<h4> .concat() </h4>"))
+
+const numbers1 = [1, 2, 3]
+const numbers2 = [4, 5]
+
+const allNumbers = numbers1.concat(numbers2)
+
+document.write("Los arrays que concatena son: "+allNumbers+ ("<br>"))
+
+//En un restaurante se reciben pedidos de comida a domicilio. 
+//Vamos a escribir una función procesarPedido que recibe un pedido, que es un array de platos. Lo que debemos hacer es:
+
+function procesarPedido(pedidos) {
+
+const cliente = pedido.shift()
+
+pedido.unshift ("bebida")
+pedido.push ("cliente")
+return pedido
+}
+
+const bedida = ("Agua", "Chocolate", "Jugo", "Gaseosa")
+
+const allPedido = nombre.concat(bedida)
+document.write("Los arrays del pedido son: "+allpedido+ ("<br>"))
